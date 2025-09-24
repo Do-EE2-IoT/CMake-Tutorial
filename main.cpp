7b83264c206c4f6f4840b067fdff21ab65ac4880
Do-EE2-IoT <do30032003@gmail.com>
2025-09-24 14:58:07 +0000

    feat: Learn target compile definintion

--- main.cpp
+++ main.cpp
@@ -6,5 +6,12 @@ int main() {
     Dog dog1(3, "Doggy", 5);
     dog1.GetDogInfo();
     dog1.bark();
+
+    #if VERSION == 1
+        std::cout << "Dog bark two times" << std::endl;
+    #else
+        std::cout << "Nothing happen" << std::endl;
+    #endif
+
     return 0;
 }
