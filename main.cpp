70cb08e63f3e8b8ddbd05b025e052e6d1c6ba0b6
Do-EE2-IoT <do30032003@gmail.com>
2025-09-26 14:49:39 +0000

    feat: use link directories

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include "dog.hpp"
 
-int main() {
+extern "C" void hello_from_other();
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
     Dog dog1(3, "Doggy", 5);
     dog1.GetDogInfo();
     dog1.bark();
 
-    #if VERSION == 1
-        std::cout << "Dog bark two times" << std::endl;
-    #else
-        std::cout << "Nothing happen" << std::endl;
-    #endif
-
+#if VERSION == 1
+    std::cout << "Dog bark two times" << std::endl;
+#else
+    std::cout << "Nothing happen" << std::endl;
+#endif
+    hello_from_other();
     return 0;
 }
